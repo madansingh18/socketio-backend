6208532a0dcfc49e805ea25861709855a7300bc4
Madan Mohan Singh <madansingh18@gmail.com>
2015-04-12 19:19:51 +0530

    5 commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Simple Socket.io Setup
  *
  * @type {exports}
- */
+
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -15,4 +15,19 @@ io.on('connection', function(socket){
 
 server.listen(app.get('port'),function(){
     console.log('Socket.io Running');
+});
+ */
+
+ var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
